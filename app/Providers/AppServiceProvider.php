d18f073128957936fae21c415b5b3aa3b46695f2
Lionel AKE <ake.kokyed@gmail.com>
2026-03-30 17:34:28 +0000

    Mise a jour de version en ligne

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Enregistrez les services de l'application.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        //
+        // Enregistrement des services dans le container d'applications
     }
 
     /**
-     * Bootstrap any application services.
+     * Effectuez les actions nécessaires après l'enregistrement des services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Définition de la longueur maximale des colonnes de type string dans la base de données
+        Schema::defaultStringLength(191);
+
+        // Autres configurations possibles
     }
 }
